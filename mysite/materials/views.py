ce826852ca6fb727d4465ee86cbcdb31bc8b35bb
PRCHR-u <158329942+PRCHR-u@users.noreply.github.com>
2025-08-14 16:43:26 +0300

    first step

---
+++ mysite/materials/views.py
@@ -0,0 +1,19 @@
+from rest_framework import generics
+from rest_framework import viewsets
+from .models import Course, Lesson
+from .serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LessonListView(generics.ListCreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
